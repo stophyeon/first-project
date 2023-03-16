a633667bd9035c1642ab803346f502c6e585bee1
jihyuk0414 <123541776+jihyuk0414@users.noreply.github.com>
2023-03-16 22:24:55 +0900

    Update jihyukproject.js
    입은 옷에 따라 포인트 활용 옷 체크 / 본인이 추위를 타나 안타나 부 추가 완료 / 오전 오후 새벽 부 추가 완료 / 간소화 완료 /
    api연결로 실제 온도 체크 부 제외 완성 . 임의 변수 있음 (html 파일 완성시 수정 요청 바람 ) 끝 ^ ^

--- front_end/jihyukproject.js
+++ front_end/jihyukproject.js
@@ -1,8 +1,8 @@
 // 간소화 완료
-// 추위 타는지 더위 타는지 , 오전 오후 저녁 버전만 추가하면 됨
+// 추위 타는지 더위 타는지 , 오전 오후 저녁 버전만 완료 
 
 
-let outerpoint, toppoint, bottompoint;
+let outerpoint, toppoint, bottompoint, dayweather;
 
 document.getElementsByName('outer').forEach(function (element) {
 element.addEventListener('click', function (event) {
@@ -20,7 +20,13 @@ document.getElementsByName('bottom').forEach(function (element) {
 element.addEventListener('click', function (event) {
 bottompoint = event.target.value;
 });
+}); //상,하,아우터 별 옷 맞추기 
+
+document.getElementsByName('day').forEach(function (element) {
+element.addEventListener('click', function (event) {
+dayweather = event.target.value;
 });
+}); //오전오후저녁변수
 
 function checkyourpoint(a) {
   if (outerpoint == 'pedding') {
@@ -30,6 +36,7 @@ function checkyourpoint(a) {
   } else if (outerpoint == 'cardigan') {
     a = a + 3;
   }
+
   if (toppoint == 'sweatshirt') {
     a = a + 4;
   } else if (toppoint == 'tshirt') {
@@ -41,13 +48,48 @@ function checkyourpoint(a) {
   } else if (bottompoint == 'shortpants') {
     a = a + 1;
   }
+  // 체크에 따른 내 포인트
 
+  if (dayweather = 'iamcold')
+  {
+    a = a + 3 
+  }
+  else if (dayweather = 'iamhot')
+  {
+    a = a - 3 
+  }
+  else if (dayweather = 'soso')
+  {
+    a = 0; 
+  } //추위를 타는지 아닌지에 대한 부  . iam cold,hot , soso는 임의 이름 //너의 포인트 셋
 
   return a;
-}; //옷별로 내 포인트 획득 부 
+}; 
 
 var hottestweatherpoint = 11; // 여기엔 api 활용 값을 넣어줘야됨 
 var coolestweatherpoint = 3;
+
+//위의 변수에 weatherpoint를 버튼 입력에 따라 변경 . 오전 오후 저녁 버전으로 
+
+function changeweatherpoint()
+{
+  if(dayweather == 'morning')
+  {
+    hottestweatherpoint = 6;  
+    coolestweatherpoint = -5 ;
+  }
+  if(dayweather == 'afternoon')
+  {
+    hottestweatherpoint = 20;  
+    coolestweatherpoint = 16 ;
+  }
+  if(dayweather == 'evening')
+  {
+    hottestweatherpoint = -3;  
+    coolestweatherpoint = -15 ;
+  }
+  
+} // 오전 오후 저녁에 따라 최고 최저기온 재설정
 
 function yourlook(yourpoint) {
   if (yourpoint >= hottestweatherpoint) {
@@ -56,12 +98,19 @@ function yourlook(yourpoint) {
     console.log('추워');
   } else if (yourpoint > coolestweatherpoint && yourpoint < hottestweatherpoint) {
     console.log('굳');
+
+
+
   } 
 }; //실제 활동시는 그림 구현
 
+
 document.getElementById('button').addEventListener('click', function(event) {
-  var yourpoint = 0;
+  var yourpoint = 0; // 추운지 더운지에 따라서 내 포인트 변경 
+  changeweatherpoint() ;
   yourpoint = checkyourpoint(yourpoint);
   yourlook(yourpoint);
   console.log(yourpoint) ;
 }); //버튼 클릭시 보이는 모습 설정 
+
+
