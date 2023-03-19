a141a2c4f6954884c13aee182c3924cb1473a145
Gyoungbokgung <123961689+Gyoungbokgung@users.noreply.github.com>
2023-03-19 11:46:58 +0900

    Update none-block.js

--- front_end/none-block.js
+++ front_end/none-block.js
@@ -3,7 +3,7 @@ const target = document.querySelector('#target'); // 표시/숨김 대상 요소
 let isShown = false; // 현재 표시되고 있는지 여부를 나타내는 변수
 
 toggleBtn.addEventListener('click', () => {
-  if (isShown) {
+  if (target.style.display !== 'none') {
     target.style.display = 'none'; // 현재 표시되고 있으면 hide로 바꿈
   } else {
     target.style.display = 'block'; // 현재 표시되고 있지 않으면 block으로 바꿈
