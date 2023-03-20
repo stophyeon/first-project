e3cb597fdf5e0b82850fbe1aab6333f29e6296ee
jihyuk0414 <123541776+jihyuk0414@users.noreply.github.com>
2023-03-20 20:44:25 +0900

    Add files via upload

---
+++ front_end/first file/mainpage.js
@@ -0,0 +1,36 @@
+var buttons = document.querySelectorAll(".weatherbutton");
+var character = document.getElementById("character"); // 이미지가 추가될 요소
+
+buttons.forEach(function(button) {
+  button.addEventListener("click", function() {
+    buttons.forEach(function(button) {
+      button.classList.remove("active");
+    });
+    button.classList.add("active");
+
+    // 이미지가 있는 경우 삭제
+    var img = character.querySelector("img");
+    if (img) {
+      character.removeChild(img);
+    }
+
+    // 이미지 추가
+    img = document.createElement('img');
+    img.src = 'character1.jpg';
+    img.style.width = "200px";
+    character.appendChild(img);
+  });
+});
+
+
+
+buttons.forEach(function(button) {
+    button.addEventListener("click", function() {
+     
+      buttons.forEach(function(button) {
+        button.classList.remove("active");
+      });
+      button.classList.add("active");
+    });
+  });
+ 
