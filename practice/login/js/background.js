ac277e6ca5955232b6073a9bca75f9e95fe60ccc
stophyeon <jeongjihyeon401@gmail.com>
2023-03-31 14:25:50 +0900

    정리해놨어

--- practice/login/js/background.js
+++
@@ -1,7 +0,0 @@
-const image = ["0.jpeg", "1.jpeg", "2.jpeg"]
-const randomimages = image[Math.floor(Math.random() * image.length)]
-const bgimg = document.createElement("img")
-
-bgimg.src = `img/${randomimages}`
-
-document.body.appendChild(bgimg)
