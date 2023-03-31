5a6b767484342491439aed8cfcb8468d4c115a83
stophyeon <jeongjihyeon401@gmail.com>
2023-03-31 14:42:21 +0900

    백엔드 작업 시작

---
+++ back_end/firtstspring/src/test/java/com/example/firtstspring/FirtstspringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.firtstspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirtstspringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
