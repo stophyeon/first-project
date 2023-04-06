8f69894579b1f411ba5dcca5dc82e68258d8fcb7
jihyuk0414 <dealon2580@naver.com>
2023-04-06 20:32:27 +0900

    옷장 표현, 메인알고리즘변경, 자잘한 부 오류 수정 ver2

--- front_end/js/main/mainpage.js
+++ front_end/js/main/mainpage.js
@@ -1,159 +1,209 @@
-var buttons = document.querySelectorAll(".weatherbutton");
-var character = document.getElementById("character"); // 이미지가 추가될 요소
+// var buttons = document.querySelectorAll(".weatherbutton");
+// var character = document.getElementById("character"); // 이미지가 추가될 요소
 
-buttons.forEach(function(button) {
-  button.addEventListener("click", function() {
-    buttons.forEach(function(button) {
-      button.classList.remove("active");
-    });
-    button.classList.add("active");
+// buttons.forEach(function(button) {
+//   button.addEventListener("click", function() {
+//     buttons.forEach(function(button) {
+//       button.classList.remove("active");
+//     });
+//     button.classList.add("active");
 
-    // 이미지가 있는 경우 삭제
-    var img = character.querySelector("img");
-    if (img) {
-      character.removeChild(img);
-    }
+//     // 이미지가 있는 경우 삭제
+//     var img = character.querySelector("img");
+//     if (img) {
+//       character.removeChild(img);
+//     }
 
-    // 이미지 추가
-    img = document.createElement('img');
-    img.src = 'character1.jpg';
-    img.style.width = "200px";
-    character.appendChild(img);
-  });
-});
+//     // 이미지 추가
+//     img = document.createElement('img');
+//     img.src = 'character1.jpg';
+//     img.style.width = "200px";
+//     character.appendChild(img);
+//   });
+// });
 
-buttons.forEach(function(button) {
-    button.addEventListener("click", function() {
+// buttons.forEach(function(button) {
+//     button.addEventListener("click", function() {
      
-      buttons.forEach(function(button) {
-        button.classList.remove("active");
-      });
-      button.classList.add("active");
-    });
-  }); //여기까지 날씨부 캐릭터 삽입
+//       buttons.forEach(function(button) {
+//         button.classList.remove("active");
+//       });
+//       button.classList.add("active");
+//     });
+//   }); //여기까지 날씨부 캐릭터 삽입
 
 
-  const clothesroompageopen = document.getElementById("clothesroompageopen");
-  const clothesroombutton = document.getElementById("clothesroom-button");
-  
-  // Add event listener to the button
-  clothesroompageopen.addEventListener("click", function() {
-    // Toggle the visibility of the div
-    if (clothesroombutton.style.display === "none") {
-      clothesroombutton.style.display = "block";
-    } else {
-      clothesroombutton.style.display = "none";
-    }
-  });
-  //옷장 누르면 밑에 옷장 나오게 하는 법 
- 
+const clothesroompageopen = document.getElementById("clothesroompageopen");
+const clothesroombutton = document.getElementById("clothesroom-button");
+
+// Add event listener to the button
+clothesroompageopen.addEventListener("click", function() {
+  // Toggle the visibility of the div
+  if (clothesroombutton.style.display === "none") {
+    clothesroombutton.style.display = "block";
+  } else {
+    clothesroombutton.style.display = "none";
+  }
+});
+//옷장 누르면 밑에 옷장 나오게 하는 법 
+
 
 
 //아래부터는 옷장 부 
-let outerpoint, toppoint, bottompoint, dayweather;
+let outerpoint, toppoint, bottompoint, etcpoint, dayweather;
 
 document.getElementsByName('outer').forEach(function (element) {
 element.addEventListener('click', function (event) {
 outerpoint = event.target.value;
+document.getElementById('yourouter').value = outerpoint;
 });
 });
 
 document.getElementsByName('top').forEach(function (element) {
 element.addEventListener('click', function (event) {
 toppoint = event.target.value;
+document.getElementById('yourtop').value = toppoint;
 });
 }); 
 
 document.getElementsByName('bottom').forEach(function (element) {
 element.addEventListener('click', function (event) {
 bottompoint = event.target.value;
+document.getElementById('yourbottom').value = bottompoint;
 });
 }); //상,하,아우터 별 옷 맞추기 
 
-document.getElementsByName('day').forEach(function (element) {
-element.addEventListener('click', function (event) {
-dayweather = event.target.value;
-});
-}); //오전오후저녁변수
+document.getElementsByName('etc').forEach(function (element) {
+  element.addEventListener('click', function (event) {
+  etcpoint = event.target.value;
+  document.getElementById('youretc').value = etcpoint;
+  });
+  }); //상,하,아우터 별 옷 맞추기 
+//여기까지 값 넣고 뭐 입었는지 뜨기 
+
+
+
+
+
+
+
 
 function checkyourpoint(a) {
+  let result = a;
+
   if (outerpoint == 'pedding') {
-    a = a + 15;
+    result = result + 12;
   } else if (outerpoint == 'coat') {
-    a = a + 7;
+    result = result + 7;
   } else if (outerpoint == 'cardigan') {
-    a = a + 3;
+    result = result + 3;
   }
+
+  // 아우터 종류에 따른 점수 추가
 
   if (toppoint == 'sweatshirt') {
-    a = a + 4;
-  } else if (toppoint == 'tshirt') {
-    a = a + 1;
+    result = result + 4;
+  } else if (toppoint == 'tshirt' || toppoint == 'shirt') {
+    result = result + 1;
   }
+
+  // 상의 종류에 따른 점수 추가
 
   if (bottompoint == 'pants') {
-    a = a + 2;
+    result = result + 2;
   } else if (bottompoint == 'shortpants') {
-    a = a + 1;
+    result = result + 1;
   }
-  // 체크에 따른 내 포인트
 
-  if (dayweather = 'iamcold')
-  {
-    a = a + 3 
+  // 하의 종류에 따른 점수 추가
+
+  if (etcpoint == 'muffler' || etcpoint == 'beanie') {
+    result = result + 1;
   }
-  else if (dayweather = 'iamhot')
-  {
-    a = a - 3 
+
+  // 기타 악세사리 종류에 따른 점수 추가
+
+  /*if (dayweather == 'iamcold') {
+    result = result + 3;
+  } else if (dayweather == 'iamhot') {
+    result = result - 3;
   }
-  else if (dayweather = 'soso')
-  {
-    a = 0; 
-  } //추위를 타는지 아닌지에 대한 부  . iam cold,hot , soso는 임의 이름 //너의 포인트 셋
+  */
 
-  return a;
-}; 
+  // 날씨에 따른 점수 추가
 
-var hottestweatherpoint = 11; // 여기엔 api 활용 값을 넣어줘야됨 
-var coolestweatherpoint = 3;
+  return result;
+}
+
+var hottestweatherpoint; // 여기엔 api 활용 값을 넣어줘야됨 
+var coolestweatherpoint;
+var averageweatherpoint ;
 
 //위의 변수에 weatherpoint를 버튼 입력에 따라 변경 . 오전 오후 저녁 버전으로 
 
-function changeweatherpoint()
-{
-  if(dayweather == 'morning')
-  {
+function changeweatherpoint() {
+  if(dayweather == 'morning') {
     hottestweatherpoint = 6;  
-    coolestweatherpoint = -5 ;
+    coolestweatherpoint = -5;
+  } else if(dayweather == 'afternoon') {
+    hottestweatherpoint = 15;  
+    coolestweatherpoint = 12;
+  } else if(dayweather == 'evening') {
+    hottestweatherpoint = -5;  
+    coolestweatherpoint = -15;
   }
-  if(dayweather == 'afternoon')
-  {
-    hottestweatherpoint = 20;  
-    coolestweatherpoint = 16 ;
-  }
-  if(dayweather == 'evening')
-  {
-    hottestweatherpoint = -3;  
-    coolestweatherpoint = -15 ;
-  }
-  
-} // 오전 오후 저녁에 따라 최고 최저기온 재설정
+  averageweatherpoint = (hottestweatherpoint + coolestweatherpoint)/ 2;
+}
+ // 오전 오후 저녁에 따라 최고 최저기온 재설정
+
+var hotimg = document.createElement("img");
+hotimg.src = "hotimg.jpg"; // 상대경로 설정
+hotimg.style.width = "300px"; // 이미지 크기 설정
+hotimg.style.height = "300px";
+document.getElementById("character").appendChild(hotimg);
 
 function yourlook(yourpoint) {
-  if (yourpoint >= hottestweatherpoint) {
-    console.log('더워');
-  } else if (yourpoint <= coolestweatherpoint) {
-    console.log('추워');
-  } else if (yourpoint > coolestweatherpoint && yourpoint < hottestweatherpoint) {
-    console.log('굳');
-  } 
+
+if (yourpoint + averageweatherpoint > 17) {
+  console.log("더워");
+  //document.getElementById("character").appendChild(hotimg); //현재 오류 
+} else if (yourpoint + averageweatherpoint < 7) {
+  console.log('추워');
+} else if (yourpoint + averageweatherpoint >= 7 && yourpoint + averageweatherpoint <= 17) {
+  console.log('굳');
+}   
+
 }; //실제 활동시는 그림 구현
 
 
 document.getElementById('submitbutton').addEventListener('click', function(event) {
-  var yourpoint = 0; // 추운지 더운지에 따라서 내 포인트 변경 
-  changeweatherpoint() ;
-  yourpoint = checkyourpoint(yourpoint);
-  yourlook(yourpoint);
-  console.log(yourpoint) ;
+var yourpoint = 0; // 추운지 더운지에 따라서 내 포인트 변경 
+changeweatherpoint() ;
+yourpoint = checkyourpoint(yourpoint);
+yourlook(yourpoint);
+console.log(averageweatherpoint)
+console.log(yourpoint + averageweatherpoint ) ;
+console.log(yourpoint);
+
 }); //버튼 클릭시 보이는 모습 설정 
+
+
+
+
+document.getElementsByName('day').forEach(function (element) {
+  element.addEventListener('click', function (event) {
+  dayweather = event.target.value;
+  console.log(dayweather);
+  changeweatherpoint();
+  console.log(averageweatherpoint);
+  });
+  }); //오전오후저녁변수
+
+// 옷 입은거 화면에 띄우기 / 완료 
+// 온도에 따른 옷 변경 / 완료
+// 캐릭터 나오기 -> 지금 사진이 깨지는 문제 발생
+// 본인 온도 부 추가 해야됨 . 
+
+ 
+// 문제점 - > 점심 처럼 20도일때, 더우니까 옷을 가볍게 입으면, 추워함 . 옷을 엄청껴입으면 , 더워함
+// 해결  
