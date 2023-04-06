5828905e265b3d8d55b0998e5ba4fe5337d8c078
stophyeon <jeongjihyeon401@gmail.com>
2023-04-06 16:04:07 +0900

    기본구조,연결완성본

--- src/main/java/com/example/firstSpring/controller/UserController.java
+++ src/main/java/com/example/firstSpring/controller/UserController.java
@@ -1,7 +1,12 @@
 package com.example.firstSpring.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class UserController {
+    @GetMapping("/")
+    public String index(){
+        return "index.html";
+    }
 }
--- src/main/java/com/example/firstSpring/repository/UserRepository.java
+++ src/main/java/com/example/firstSpring/repository/UserRepository.java
@@ -1,4 +1,4 @@
-package com.example.firstSpring.repository.entity;
+package com.example.firstSpring.repository;
 
 import com.example.firstSpring.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
