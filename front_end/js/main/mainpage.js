3c3070e0bd667680b54aef47f9fb8ae75c039866
Sunggyu Han <gkstjdrb00@soongsil.ac.kr>
2023-04-09 20:40:39 +0900

    뿌우떵규와쪄염

--- front_end/js/main/mainpage.js
+++ front_end/js/main/mainpage.js
@@ -94,6 +94,8 @@ document.getElementsByName('etc').forEach(function (element) {
 
 
 
+
+
 function checkyourpoint(a) {
   let result = a;
 
@@ -162,14 +164,14 @@ function changeweatherpoint() {
 }
  // 오전 오후 저녁에 따라 최고 최저기온 재설정
 
- 
- var hotimg = document.createElement('img');
+  
+ let hotimg = document.createElement('img');
  hotimg.src = '../main/hotimg.jpg';
- var coldimg = document.createElement('img')
+ let coldimg = document.createElement('img')
  coldimg.src = '../main/coldimg.jpg'
- var goodimg = document.createElement('img')
+ let goodimg = document.createElement('img')
  goodimg.src = '../main/goodimg.jpg'
- 
+
  var character = document.getElementById("character");
  
  function yourlook(yourpoint) {
@@ -247,3 +249,4 @@ nightButton.addEventListener("click", () => {
 // 문제점 - > 점심 처럼 20도일때, 더우니까 옷을 가볍게 입으면, 추워함 . 옷을 엄청껴입으면 , 더워함
 // 해결  
 // 성규꺼 사라짐
+
