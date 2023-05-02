65212ed45fd9548f7238f29573b2a7a223ba1765
dntmdals <a01030521713@gmail.com>
2023-05-02 15:41:29 +0900

    mysql의존성추가

--- src/main/java/com/example/firstSpring/controller/UserController.java
+++ src/main/java/com/example/firstSpring/controller/UserController.java
@@ -1,15 +1,35 @@
 package com.example.firstSpring.controller;
+import com.example.firstSpring.entity.User;
+import com.example.firstSpring.repository.UserRepository;
 import com.example.firstSpring.service.UserService;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 @Controller
 public class UserController {
     @Autowired
-    UserService userService;
-    public void user(){
+    private UserService userService;
+    private UserRepository userRepository;
+    @PostMapping(value = "/login")
+    public String login(@RequestParam String username, @RequestParam String password, HttpSession session){
+
+        User user = userService.findByUsername(username);
+        if (user != null && user.getPassword().equals(password)) {
+            session.setAttribute("login", login);
+            return "redirect:/main";
+        } else {
+            return "redirect:/login";
+        }
 
     }
+
+
 
 
 }
--- src/main/java/com/example/firstSpring/service/UserService.java
+++ src/main/java/com/example/firstSpring/service/UserService.java
@@ -18,4 +18,6 @@ public class UserService  {
 
     UserRepository userRepository;
 
+
+
 }
