744837ac5d74b072ed4441c7916eb6a763a7fb96
stophyeon <jeongjihyeon401@gmail.com>
2023-05-22 13:09:11 +0900

    완성본

--- src/main/java/com/example/firstSpring/Repository/UserRepository.java
+++ src/main/java/com/example/firstSpring/Repository/UserRepository.java
@@ -6,6 +6,7 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -13,9 +14,5 @@ public interface UserRepository extends JpaRepository<User, Long> {
 
     @Query("SELECT u FROM User u WHERE u.userid = :userid")
     User findByUserid(@Param("userid") String userid);
-    /*
-    @Query("SELECT u.password FROM User u WHERE u.userid = :userid")
-    User findPasswordByUserId(@Param("userid") User userId);
-    */
-    }
+}
 
